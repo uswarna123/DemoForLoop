72cd0f95b2a0b57f92ccca7b64eccf316a12732e
uswarna123 <109545573+uswarna123@users.noreply.github.com>
2024-02-15 19:43:18 -0500

    SumOfNumbers.java

--- MultiplicationTableForANumber.java
+++ MultiplicationTableForANumber.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class MultiplicationTableForANumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter a positive integer");
+        int num;
+        num= sc.nextInt();
+        for (int i=1; i<=10 ; i++)
+        {
+            System.out.println(num+" * "+i+" = "+num*i);
+        }
+
+    }
 }
